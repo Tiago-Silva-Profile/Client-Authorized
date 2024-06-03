8c795787c6e84374229310871d38bd8191a00608
Tiago.Antonio <tiago.antonio@mv.com.br>
2024-06-03 13:51:29 -0300

    [AUT-01] Inicio da aplicação

---
+++ src/main/java/com/authorized/client_authorized/ClientAuthorizedApplication.java
@@ -0,0 +1,13 @@
+package com.authorized.client_authorized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAuthorizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAuthorizedApplication.class, args);
+	}
+
+}
